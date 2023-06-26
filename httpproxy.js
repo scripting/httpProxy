c74327cc48747f9058ae62b9cb1ba80045b84a96
Dave Winer <dave.winer@gmail.com>
2023-06-26 13:24:54 -0400

    .

--- httpproxy.js
+++ httpproxy.js
@@ -7,27 +7,87 @@ const utils = require ("daveutils");
 var config = {
 	port: process.env.PORT || 5393,
 	flLogToConsole: true,
+	flPostEnabled: true,
 	flAllowAccessFromAnywhere: true
 	}
 
+
+
+
 function handleHttpRequest (theRequest) {
 	const params = theRequest.params, whenstart = new Date ();
-	switch (theRequest.lowerpath) {
-		case "/httpreadurl":
-			request (params.url, function (err, response, body) {
-				if (err) {
-					theRequest.httpReturn (500, "text/plain", err.message);
-					}
-				else {
-					theRequest.httpReturn (response.statusCode, "text/plain", body);
-					}
-				});
+	function return404 () {
+		theRequest.httpReturn (404, "text/plain", "Not found.");
+		}
+	function returnPlainText (data) {
+		theRequest.httpReturn (200, "text/plain", data);
+		}
+	function returnData (jstruct) {
+		if (jstruct === undefined) {
+			jstruct = {};
+			}
+		theRequest.httpReturn (200, "application/json", utils.jsonStringify (jstruct));
+		}
+	function returnError (jstruct) {
+		theRequest.httpReturn (500, "application/json", utils.jsonStringify (jstruct));
+		}
+	function httpReturn (err, jstruct) {
+		if (err) {
+			returnError (err);
+			}
+		else {
+			returnData (jstruct);
+			}
+		}
+	switch (theRequest.lowermethod) {
+		case "get":
+			switch (theRequest.lowerpath) {
+				case "/httpreadurl":
+					request (params.url, function (err, response, body) {
+						if (err) {
+							theRequest.httpReturn (500, "text/plain", err.message);
+							}
+						else {
+							theRequest.httpReturn (response.statusCode, "text/plain", body);
+							}
+						});
+					break;
+				case "/now":
+					theRequest.httpReturn (200, "text/plain", new Date ());
+					break;
+				default:
+					return404 ();
+					break;
+				}
 			break;
-		case "/now":
-			theRequest.httpReturn (200, "text/plain", new Date ());
-			break;
-		default:
-			theRequest.httpReturn (404, "text/plain", "Not found.");
+		case "post":
+			switch (theRequest.lowerpath) {
+				case "/httppost": //6/26/23 by DW
+					var myRequest = {
+						method: "POST",
+						url: params.url,
+						followRedirect: true, 
+						body: theRequest.postBody
+						};
+					request (myRequest, function (err, response, data) {
+						if (err) {
+							returnError (err);
+							}
+						else {
+							if (response.statusCode == 200) {
+								returnPlainText (data);
+								}
+							else {
+								const message = "http response code == " + response.statusCode;
+								returnError ({message});
+								}
+							}
+						});
+					return (true);
+				default:
+					return404 ();
+					break;
+				}
 			break;
 		}
 	}
